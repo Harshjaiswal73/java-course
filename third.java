d03cfdc283adb51aa7c73ff0531e7c2326694377
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2024-12-27 11:36:44 +0000

    third

---
+++ third.java
@@ -0,0 +1,24 @@
+public class third{
+    public static void main(String[] args){
+        // whole numbers
+        // byte
+        // short
+        // int
+        // long
+
+        
+        byte age1 = 20;
+        short age3 =20;
+        int age = 20;
+        long age4 = 20;
+        
+
+        // decimal numbers
+            // float
+            // double
+        //characters
+           // char
+        // booleans
+          //
+    }
+}
