c8c9973e60b82003c30eaf79fe1150b0cf06a772
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2024-12-28 18:47:15 +0000

    widening and Narrowing and Automatic conversion in java

---
+++ demo2.java
@@ -0,0 +1,35 @@
+public class demo2 {
+    public static void main(String[]args)
+    {
+          int a = 10;  // 4 bytes
+          long b = a;  // 8 bytes
+          float c = a; // 4 bytes
+
+          double doubleValue = 123.456;
+          float floatValue = (float) doubleValue; // Narrowing conversion from double to float
+          long longValue = (long) floatValue;// Narrowing conversion from float to long
+          int intValue = (int) longValue; // Narrowing conversion from long to int
+
+          System.out.println(a);
+          System.out.println(b);
+          System.out.println(c);
+
+
+          float f =1.7f;
+          int g = (int) f; // widening conversion or automatic conversion from float to int
+          System.out.println(g);
+
+          byte byteValue = 10; // 1 byte
+          short shortValue = byteValue; //2 bytes
+          int intValue = shortValue; // 4 bytes
+          long longValue = intValue; //8 bytes
+          float floatValue = longValue; //4 bytes
+          double doubleValue = floatValue; // 8 bytes
+
+          System.out.println("int" + intValue);
+          System.out.println("long" + longValue);
+          System.out.println("float" + floatValue);
+          System.out.println("double" + doubleValue);
+
+    }
+}
