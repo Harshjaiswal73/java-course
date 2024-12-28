732746bbf63e6c5707672ff691fa76a6dd8621e3
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2024-12-28 18:22:09 +0000

    assci

---
+++ char.java
@@ -0,0 +1,11 @@
+class Char{
+    public static void main(String[]args)
+    {
+        char initial = 'H'; // char using for the single charracter and number symbols
+        // change to integer
+        System.out.println((int)Character.MIN_VALUE);
+        System.out.println((int)Character.MAX_VALUE);
+        char heartSymbol =(char) 10084;
+        System.out.println(heartSymbol);
+    }
+}
