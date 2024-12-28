d50fa2d81929c837cb70e731dba1a2776dcb6ade
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2024-12-28 17:49:25 +0000

    range

---
+++ fourth.java
@@ -0,0 +1,9 @@
+class fourth{
+    public static void main(String []args)
+    {
+        float mySalary = 10000.2423423242424242424f;
+        double yourSalary = 232324.2204020402402034020402030204320432;
+        System.out.println(mySalary);
+        System.out.println(yourSalary);
+    }
+}
