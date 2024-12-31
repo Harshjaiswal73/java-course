1af52f6b6e0f6540306b4cc0848ecd25d2194e67
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2024-12-31 02:52:26 +0000

    varriables conditions

---
+++ varriables.java
@@ -0,0 +1,12 @@
+class varriables{
+    public static void main(String[]args)
+    {
+        // case senstive
+        // can Be Letters, Digits,Dollar Signs, or underscores
+        // must Begin with a Letter,Dollar sign ($),or Underscores (_)
+        // cannot use java keywords as varriable names
+        String myName = "Harsh Jaiswal ";
+        System.out.println(myName);
+
+    }
+}
