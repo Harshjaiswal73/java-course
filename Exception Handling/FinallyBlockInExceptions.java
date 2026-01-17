6a87c6244dabf52fdb03ad1748913d223d3cd7d8
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2026-01-17 18:07:30 +0000

    finally Exceptions

---
+++ Exception Handling/FinallyBlockInExceptions.java
@@ -0,0 +1,20 @@
+public class FinallyBlockInExceptions {
+
+    public static void main(String[] args) {
+        int a[] = new int[5];
+
+        System.out.println("Hello World");
+
+        try {
+            System.out.println(a[8]);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exceptions Handled");
+        }finally{
+            System.out.println("I will run always");
+        }
+
+        System.out.println(" bye world");
+    }
+    
+}
