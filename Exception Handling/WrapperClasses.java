5af6ff251dec57f1f9b98a12ad5fbd6fd934b150
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2026-01-20 16:55:33 +0000

    update

---
+++ Exception Handling/WrapperClasses.java
@@ -0,0 +1,11 @@
+public class WrapperClasses {
+
+    public static void main(String []args){
+        Integer obj = new Integer(12);
+        Integer obj2 = Integer.valueOf(12);
+
+        Integer obj3  = 12; // autoboxing 
+
+        int age = obj; // unboxing
+    }
+}
