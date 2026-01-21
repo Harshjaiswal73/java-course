4e272e23baa240c8342036c9816f76b734e14a95
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2026-01-21 17:19:02 +0000

    Exception handling and wrapper and generic class complete notes

--- Exception Handling/WrapperClasses.java
+++ Exception Handling/WrapperClasses.java
@@ -1,8 +1,14 @@
 public class WrapperClasses {
 
     public static void main(String []args){
+
         Integer obj = new Integer(12);
-        Integer obj2 = Integer.valueOf(12);
+
+        Integer obj2 = Integer.valueOf("12");
+
+        System.out.println(obj2*4);
+
+        Boolean myBoolean = Boolean.valueOf("false");
 
         Integer obj3  = 12; // autoboxing 
 
